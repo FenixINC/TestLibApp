849e8a897fc8739caa18c600851424955b590dc3
taras_koloshmatin <t.koloshmatin@lonwood.software>
2021-08-13 01:14:47 +0300

    Initial commit

---
+++ testlib/src/main/java/com/inquisitor/testlib/TestClass.kt
@@ -0,0 +1,14 @@
+package com.inquisitor.testlib
+
+class TestClass {
+
+    private val testList = mutableListOf(
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five"
+    )
+
+    fun getTestList() = testList
+}
